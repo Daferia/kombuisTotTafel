f2fc29091f2c82dcdb23c6e847837d12ef679b7c
Fabio Faria <daferia@gmail.com>
2022-08-07 00:35:37 +0100

    add materialboxed for recipe images

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Sidenav initiator
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav, {edge:"right"});
 
-  document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav, {edge:"right"});
-  });
+  // Effect on hovering over the recipe images
+  let materialboxed = document.querySelectorAll('.materialboxed');
+  M.Materialbox.init(materialboxed);    
+});
