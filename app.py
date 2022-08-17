5fb05d0db6e1e3b62cd5057bc5870adde276261c
Fabio Faria <daferia@gmail.com>
2022-08-17 22:30:24 +0100

    add delete and add_recipe functions

--- app.py
+++ app.py
@@ -36,6 +36,28 @@ def view_recipe(recipe_id):
     return render_template("view_recipe.html", recipe=recipe)
 
 
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_description": request.form.get("recipe_description"),
+            "category_name": request.form.get("category_name"),
+            "prep_time": request.form.get("prep_time"),
+            "cooking_time": request.form.get("cooking_time"),
+            "servings": request.form.get("servings"),
+            "ingredients": request.form.get("ingredients"),
+            "method": request.form.get("method"),
+            "image_url": request.form.get("image_url")
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe has been Added")
+        return redirect(url_for("recipes"))
+    
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("add_recipe.html", categories=categories)
+
+
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
@@ -50,14 +72,22 @@ def edit_recipe(recipe_id):
             "servings": request.form.get("servings"),
             "ingredients": request.form.get("ingredients"),
             "method": request.form.get("method"),
+            "image_url": request.form.get("image_url")
         }}
         mongo.db.recipes.update_one({"_id": ObjectId(recipe_id)}, submit)
         flash("Recipe has been updated")
         return redirect(url_for("view_recipe", recipe_id=recipe_id))
     
-    categories = mongo.db.categories.find().sort("category_name", 1)
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
     return render_template("edit_recipe.html", recipe=recipe, categories=categories)
 
+
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.delete_one({'_id': ObjectId(recipe_id)})
+    flash("Recipe has been deleted")
+    return redirect(url_for("recipes"))
+    
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
