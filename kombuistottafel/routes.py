bc64e557c46155e52c96568a27e8252cbef94bee
Fabio Faria <daferia@gmail.com>
2022-08-20 19:33:46 +0000

    change categories query from mongo to postgres

--- kombuistottafel/routes.py
+++ kombuistottafel/routes.py
@@ -48,7 +48,7 @@ def add_recipe():
         flash("Recipe has been Added")
         return redirect(url_for("recipes"))
     
-    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("add_recipe.html", categories=categories)
 
 
@@ -72,7 +72,7 @@ def edit_recipe(recipe_id):
         flash("Recipe has been updated")
         return redirect(url_for("view_recipe", recipe_id=recipe_id))
 
-    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("edit_recipe.html", recipe=recipe, categories=categories)
 
 
@@ -81,3 +81,114 @@ def delete_recipe(recipe_id):
     mongo.db.recipes.delete_one({'_id': ObjectId(recipe_id)})
     flash("Recipe has been deleted")
     return redirect(url_for("recipes"))
+
+
+@app.route("/get_categories")
+def get_categories():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("get_categories"))
+    return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("get_categories"))
+    return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    mongo.db.tasks.delete_many({"category_id": str(category_id)})
+    return redirect(url_for("get_categories"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = Users.query.filter(Users.user_name == \
+                                           request.form.get("username").lower()).all()
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        user = Users(
+            user_name=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = Users.query.filter(Users.user_name == \
+                                           request.form.get("username").lower()).all()
+
+        if existing_user:
+            print(request.form.get("username"))
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user[0].password, request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = Users.query.filter(Users.user_name == session["user"]).all()
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
