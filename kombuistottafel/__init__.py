d6f20eb1c3b28196c1e523f8c435a38c55d1aa21
Fabio Faria <daferia@gmail.com>
2022-08-20 20:01:46 +0000

    update __init__.py to accomodate the heroku db as well as local

--- kombuistottafel/__init__.py
+++ kombuistottafel/__init__.py
@@ -10,10 +10,16 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
-uri = os.environ.get("DB_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+    # local DB
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  
+else:
+    # heroku DB
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  
 
 db = SQLAlchemy(app)
 
